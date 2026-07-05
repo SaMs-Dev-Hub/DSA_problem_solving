848f5801ea25a02332beadb8d0e63f7a7f7f2916
Samir Avhad <104997385+SaMs-Dev-Hub@users.noreply.github.com>
2026-07-06 00:08:17 +0530

    Time: 0 ms (100%), Space: 58.8 MB (12.32%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -4,12 +4,11 @@
  */
 var removeDuplicates = function(nums) {
     let j=0;
-    for(let i=0;i<nums.length;i++){
-        if(nums[j]!== nums[i]){
-            nums[j+1]=nums[i]
+    for(let i=1;i<nums.length;i++){
+        if(nums[j] != nums[i]){
+            nums[j+1]=nums[i];
             j++
         }
     }
-  
     return j+1
 };
