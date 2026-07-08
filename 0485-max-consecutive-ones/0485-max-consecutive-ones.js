aff3963b3be995587f3839ba81126357bb41fe0e
Samir Avhad <104997385+SaMs-Dev-Hub@users.noreply.github.com>
2026-07-08 23:56:47 +0530

    Time: 0 ms (100%), Space: 59.5 MB (55.57%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+          let maxCount=0;
+    let count=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===1){
+            count++
+            if(count>maxCount){
+                maxCount=count
+            }
+        }else{
+            count=0
+        }
+    }
+    return maxCount
+};
